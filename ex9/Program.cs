8a57dc8d8fc4b92a8b0c2d99bea64c2b2df26609
Alexey <alexey25ya@mail.ru>
2022-07-01 22:12:59 +0300

    seminar3

---
+++ ex9/Program.cs
@@ -0,0 +1,23 @@
+﻿void Farray(int[] collect)
+{
+    int length = collect.Length;
+    int i=0;
+    while(i<length)
+    {
+        collect[i]=new Random().Next(1,100);
+        i++;
+    }
+}
+void Parray(int[] col)
+{
+    int l=col.Length;
+    int i1=0;
+    while (i1<l)
+    {
+        Console.WriteLine(col[i1]);
+        i1++;
+    }
+}
+int[] array = new int[10];
+Farray(array);
+Parray(array);
---
+++ семинар3/2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("vvedite nomer chetverti");
+int n1 = Convert.ToInt32(Console.ReadLine());
+string Func(int n)
+{
+    switch (n)
+
+    {
+        case 1:
+            {
+                return "x>0,y>0";
+                break;
+            }
+        case 2:
+            {
+                return "x<0,y>0";
+                break;
+            }
+        case 3:
+            {
+                return "x<0,y<0";
+                break;
+            }
+        case 4:
+            {
+                return "x>0,y<0";
+                break;
+            }
+        default:
+            {
+
+                {
+                    return "err";
+                    break;
+
+                }
+            }
+    }
+}
+Console.WriteLine(Func(n1));
