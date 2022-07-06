0f94533b006515d67e89ec7186c7fd195677374f
Alexey <alexey25ya@mail.ru>
2022-07-06 17:36:52 +0300

    лекция3

---
+++ лекция3/ex3.1/Program.cs
@@ -0,0 +1,32 @@
+﻿// отсортировать массив
+
+int[] arr={1,5,4,3,2,6,7,1,1};
+void PrintArray(int[] array)
+{
+    int count=array.Length;
+    for(int i = 0; i <count;i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void Sort(int[] array)
+{
+    for(int i=0;i<array.Length-1;i++)
+    {
+        int minPos =i;
+        for(int j=i+1;j<array.Length;j++)
+        {
+            if(array[j]<array[minPos]) minPos=j;
+        }
+        int temp=array[i];
+        array[i]=array[minPos];
+        array[minPos]=temp;
+    }
+}
+PrintArray(arr);
+Sort(arr);
+
+
+PrintArray(arr);
