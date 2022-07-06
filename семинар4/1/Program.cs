31f2c9548b7e42d89d2c99792c33ca276e6d54ac
Alexey <alexey25ya@mail.ru>
2022-07-06 18:37:39 +0300

    семинар4

---
+++ семинар4/1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход 
+//число (А) и выдаёт сумму чисел от 1 до А.
+
+int GetLength(int num)
+{
+
+int i=0;
+while(num>0)
+{
+i++;
+num/=10;
+}
+return i;
+}
+int size = GetLength(766386);
+
+Console.WriteLine (size);
---
+++ семинар4/3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+int[] fillArray (int[] arr)
+{
+int length = arr.Length;
+int index = 0;
+while (index < length)
+{
+arr[index] = new Random().Next(0, 2);
+index++;
+}
+return arr;
+}
+
+void printArray (int[] arr1)
+{
+int count = arr1.Length;
+int position = 0;
+while (position < count)
+{
+Console.Write(arr1[position] + "|");
+position++;
+}
+Console.WriteLine();
+}
+
+int[] array = new int[8];
+
+fillArray(array);
+
+
+printArray(array);
+
+//Console.Write(String.Join("|",array));
+//Console.WriteLine();
