fc9abb6322b8cdd04f928002f95117f21eb4917d
Alexey <alexey25ya@mail.ru>
2022-07-09 12:34:30 +0300

    семинар5

--- семинар3/2/Program.cs
+++ семинар3/2/Program.cs
@@ -11,29 +11,29 @@ string Func(int n)
         case 1:
             {
                 return "x>0,y>0";
-                break;
+                
             }
         case 2:
             {
                 return "x<0,y>0";
-                break;
+                
             }
         case 3:
             {
                 return "x<0,y<0";
-                break;
+                
             }
         case 4:
             {
                 return "x>0,y<0";
-                break;
+                
             }
         default:
             {
 
                 {
                     return "err";
-                    break;
+                    
 
                 }
             }
