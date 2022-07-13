95c0c8bad2423b94c984aaf29bedadf69871f25a
Alexey <alexey25ya@mail.ru>
2022-07-13 20:04:30 +0300

    семинар6

---
+++ семинар6/2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+//может ли существовать треугольник с сторонами такой длины.
+
+bool IsTriangle(int a, int b,int c)
+{
+    if(a+b>c && a+c>b && b+c>a)
+    {
+        return true;
+    }
+    return false;
+}
+Console.WriteLine(IsTriangle(5,4,7));
